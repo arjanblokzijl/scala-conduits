7de162066299a02dc3c09ce93defe0cad9df4a36
Arjan Blokzijl <arjanblokzijl@gmail.com>
2012-02-13 19:49:07 +0100

    functor instance for Sink

--- conduits/src/main/scala/conduits/Sink.scala
+++ conduits/src/main/scala/conduits/Sink.scala
@@ -1,28 +1,14 @@
 package conduits
 
-import scalaz.{MonadTrans, Functor, Monad}
+import scalaz.{Functor, Monad}
 
+import sinks._
+import resource._
 
-//data PreparedSink input m output =
-//    SinkNoData output
-//  | SinkData
-//        { sinkPush :: input -> ResourceT m (SinkResult input output)
-//        , sinkClose :: ResourceT m output
-//        }
-sealed trait PreparedSink[I, F[_], O] {
-
-
-}
+sealed trait PreparedSink[I, F[_], O]
 case class SinkNoData[I, F[_], O](output: O) extends PreparedSink[I, F, O]
-//trait SinkData[I, F[_], O] extends PreparedSink[I, F, O] {
-//  def sinkPush(input: I): ResourceT[F, SinkResult[I, O]]
-//  def sinkClose: ResourceT[F, SinkResult[I, O]]
-//}
-case class SinkData[I, F[_], O](sinkPush: I => ResourceT[F, SinkResult[I, O]],
-                                sinkClose: ResourceT[F, SinkResult[I, O]]) extends PreparedSink[I, F, O] {
-
-}
-
+case class SinkData[I, F[_], O](sinkPush: SinkPush[I, F, O],
+                                sinkClose: SinkClose[I, F, O]) extends PreparedSink[I, F, O]
 
 trait SinkResult[I, O] {
   def map[B](f: O => B) = this match {
@@ -33,10 +19,14 @@ trait SinkResult[I, O] {
 case class Processing[I, O]() extends SinkResult[I, O]
 case class Done[I, O](input: Option[I], output: O) extends SinkResult[I, O]
 
-case class Sink[I, F[_], O](prepare: ResourceT[F, PreparedSink[I, F, O]])
+case class Sink[I, F[_], O](prepare: ResourceT[F, PreparedSink[I, F, O]]) {
+//  def map[B](f: O => B) = {
+//    resourceTMonad[F].map[PreparedSink[I, F, O], PreparedSink[I, F, B]](prepare)((r: PreparedSink[I, F, O]) => sys.error(""))
+//  }
+}
 
 trait SinkInstances {
-  import resource._
+
   implicit def sinkResultFunctor[I]: Functor[({type l[a] = SinkResult[I, a]})#l] = new Functor[({type l[a] = SinkResult[I, a]})#l] {
     def map[A, B](fa: SinkResult[I, A])(f: (A) => B): SinkResult[I, B] = fa match {
       case Processing() => Processing[I, B]()
@@ -48,10 +38,16 @@ trait SinkInstances {
     def map[A, B](fa: PreparedSink[I, F, A])(f: (A) => B): PreparedSink[I, F, B] = fa match {
       case SinkNoData(o) => SinkNoData(f(o))
       case SinkData(p, c) => SinkData(sinkPush = i =>
-                                        resourceTMonad[F].map[SinkResult[I, A], SinkResult[I, B]](p(i))((r: SinkResult[I, A]) => r.map(f)),
+                                                  resourceTMonad[F].map[SinkResult[I, A], SinkResult[I, B]](p(i))((r: SinkResult[I, A]) => r.map(f)),
                                       sinkClose = resourceTMonad[F].map[SinkResult[I, A], SinkResult[I, B]](c)((r: SinkResult[I, A]) => r.map(f))
                                       )
     }
+  }
+
+  implicit def sinkFunctor[I, F[_], O](implicit M: Monad[F]): Functor[({type l[a] = Sink[I, F, a]})#l] = new Functor[({type l[a] = Sink[I, F, a]})#l] {
+    def map[A, B](fa: Sink[I, F, A])(f: (A) => B): Sink[I, F, B] =
+      Sink(resourceTMonad[F].map[PreparedSink[I, F, A], PreparedSink[I, F, B]](fa.prepare)(r =>
+             preparedSinkFunctor[I, F].map(r)(a => f(a))))
   }
 }
 

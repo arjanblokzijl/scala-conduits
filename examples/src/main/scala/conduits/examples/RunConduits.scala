352fb30ab8be53d9ea7cfba64ea60b738efce6d4
Arjan Blokzijl <arjanblokzijl@gmail.com>
2012-03-06 18:49:19 +0100

    runResourceT example

--- examples/src/main/scala/conduits/examples/RunConduits.scala
+++ examples/src/main/scala/conduits/examples/RunConduits.scala
@@ -3,14 +3,19 @@ package examples
 
 import scalaz.effect.IO
 import resourcet._
-import resource._
-//import ResourceT
+import resource.{resourceTMonad, runResourceT, resourceTMonadBaseIo}
 
 object RunConduits extends App {
   val sink = CL.sumSink[IO]
+  val sinkT = CL.sumSink[({type l[a] = ResourceT[IO, a]})#l]
   val source = CL.sourceList[IO, Int]((1 to 10).toStream)
+  val sourceT = CL.sourceList[({type l[a] = ResourceT[IO, a]})#l, Int]((1 to 10).toStream)
+
 
   val rt: IO[Int] = source >>== sink
+  val rt2 = sourceT >>== sinkT
 
-  println("result " + rt.unsafePerformIO)
+
+  println("result simple " + rt.unsafePerformIO)
+  println("result resourceT " + runResourceT(rt2).unsafePerformIO)
 }

4acd6a1f37a4e501b40e87e59368fb2e2f5c9023
Arjan Blokzijl <arjanblokzijl@gmail.com>
2012-03-18 15:04:19 +0100

    add conduitIO

--- conduits/src/main/scala/conduits/ConduitUtil.scala
+++ conduits/src/main/scala/conduits/ConduitUtil.scala
@@ -3,12 +3,26 @@ package conduits
 import scalaz.Monad
 import Conduit._
 import Source._
+import scalaz.effect.IO
+import resourcet.{ReleaseKey, MonadResource}
 
 /**
  * User: arjan
  */
 trait ConduitStateResult[S, A, B] {
   def fold[Z](finished: (=> Option[A], => Stream[B]) => Z, producing: (=> S, => Stream[B]) => Z): Z
+}
+
+//-- | A helper type for @conduitIO@, indicating the result of being pushed to.
+//-- It can either indicate that processing is done, or to continue.
+//--
+//-- Since 0.2.0
+//data ConduitIOResult input output =
+//    IOFinished (Maybe input) [output]
+//  | IOProducing [output]
+//
+trait ConduitIOResult[A, B] {
+  def fold[Z](finished: (=> Option[A], => Stream[B]) => Z, producing: (=> Stream[B]) => Z): Z
 }
 
 object ConduitUtil {
@@ -24,6 +38,20 @@ object ConduitUtil {
       def fold[Z](finished: (=> Option[A], => Stream[B]) => Z, producing: (=> S, => Stream[B]) => Z) = producing(state, output)
     }
     def unapply[S, A, B](r: ConduitStateResult[S, A, B]): Option[(S, Stream[B])] = r.fold(ToNone2, (s, o) => Some(s, o))
+  }
+
+  object IOFinished {
+    def apply[A, B](maybeInput: => Option[A], output: => Stream[B]) = new ConduitIOResult[A, B] {
+      def fold[Z](finished: (=> Option[A], => Stream[B]) => Z, producing: (=> Stream[B]) => Z) = finished(maybeInput, output)
+    }
+    def unapply[A, B](r: ConduitIOResult[A, B]): Option[(Option[A], Stream[B])] = r.fold((i, o) => Some(i, o), ToNone)
+  }
+
+  object IOProducing {
+    def apply[A, B](output: => Stream[B]) = new ConduitIOResult[A, B] {
+      def fold[Z](finished: (=> Option[A], => Stream[B]) => Z, producing: (=> Stream[B]) => Z) = producing(output)
+    }
+    def unapply[A, B](r: ConduitIOResult[A, B]): Option[(Stream[B])] = r.fold(ToNone2, Some(_))
   }
 
   /**
@@ -42,6 +70,32 @@ object ConduitUtil {
      , producing = (state, output) => haveMore[A, F, B](Running[A, F, B](push1(state), close1(state)), M.point(()), output)
     )
     Running(push1(state), close1(state))
+  }
+
+  /*Construct a Conduit*/
+  def conduitIO[F[_], A, B, S](alloc: IO[S], cleanup: S => IO[Unit], push: S => A => F[ConduitIOResult[A, B]], close: S => F[Stream[B]])(implicit M0: MonadResource[F]): Conduit[A, F, B] = {
+    implicit val M = M0.MO
+    def push1(key: => ReleaseKey, state: => S, input: => A): F[Conduit[A, F, B]] = {
+      M.bind(push(state)(input))(res => res.fold(
+        finished = (leftover, output) => M.bind(M0.release(key))(_ => M.point(haveMore(Finished(leftover), M.point(()), output)))
+        ,
+        producing = output => M.point(haveMore(
+          Running(i => ConduitM(push1(key, state, i), M0.release(key)), close1(key, state))
+          , M.bind(M0.release(key))(_ => M.point(()))
+          , output
+        ))))
+
+    }
+    def close1(key: ReleaseKey, state: S): Source[F, B] = {
+      SourceM(M.bind(close(state))(output => M.bind(M0.release(key))(_ => M.point(fromList(output)))), M0.release(key))
+    }
+    Running(push = input =>
+              ConduitM(M.bind(M0.allocate(alloc, cleanup))(ks =>
+                push1(ks._1, ks._2, input)), M.point(())),
+             close = SourceM(M.bind(M0.allocate(alloc, cleanup))(ks =>
+               M.bind(close(ks._2))(os =>
+                 M.bind(M0.release(ks._1))(_ => M.point(fromList(os))))), M.point(())
+               ))
   }
 
   def haveMore[A, F[_], B](res: Conduit[A, F, B], close: F[Unit], bs: Stream[B])(implicit M: Monad[F]): Conduit[A, F, B] = bs match {

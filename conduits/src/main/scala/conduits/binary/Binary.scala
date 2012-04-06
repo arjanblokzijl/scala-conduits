03936977778f259b7a798a4427ef0d607c98eb8f
Arjan Blokzijl <arjanblokzijl@gmail.com>
2012-04-06 15:42:05 +0200

    untested sourceFileRange

--- conduits/src/main/scala/conduits/binary/Binary.scala
+++ conduits/src/main/scala/conduits/binary/Binary.scala
@@ -7,8 +7,9 @@ import SourceFunctions._
 import ConduitFunctions._
 import SinkFunctions._
 import scalaz.effect.IO
-import resourcet.MonadResource
 import java.io.{FileOutputStream, FileInputStream, File}
+import java.nio.channels.FileChannel
+import resourcet.{ReleaseKey, MonadResource}
 
 /**
  * User: arjan
@@ -27,6 +28,31 @@ object Binary {
         if (bs.isEmpty) IOClosed.apply
         else IOOpen(bs))
     })
+
+  def sourceFileRange[F[_]](f: File, offset: Option[Int] = None, count: Option[Int] = None, chunkSize: Int = byteString.DefaultChunkSize)(implicit MR: MonadResource[F]): Source[F, ByteString] = {
+    val M = MR.MO
+    def pullUnlimited(c: FileChannel, key: ReleaseKey): F[Source[F, ByteString]] =
+      MR.MO.bind(MR.MO.liftIO(byteString.getContents(c)))(bs =>
+        if (bs.isEmpty) M.map(MR.release(key))(_ => Done[Zero, ByteString, F, Unit](None, ()))
+        else M.point(HaveOutput(PipeM(pullUnlimited(c, key), MR.release(key)), MR.release(key), bs))
+      )
+
+    def pullLimited(i: Int, fc: FileChannel, key: ReleaseKey): F[Source[F, ByteString]] = {
+      val c = math.min(i, byteString.DefaultChunkSize)
+      MR.MO.bind(MR.MO.liftIO(byteString.getContents(fc, c)))(bs => {
+        val c1 = c - bs.length
+        if (bs.isEmpty) M.map(MR.release(key))(_ => Done[Zero, ByteString, F, Unit](None, ()))
+        else M.point(HaveOutput(PipeM(pullLimited(c1, fc, key), MR.release(key)), MR.release(key), bs))
+      })
+    }
+
+    PipeM(M.bind(MR.allocate[FileChannel](IO(new FileInputStream(f).getChannel), c => IO(c.close)))(kh => {
+      val key = kh._1
+      val chan = kh._2
+      M.bind(offset.map(off => M.liftIO(IO(kh._2.position(off)))) getOrElse M.point(()))(_ =>
+        count.map(o => pullLimited(o, chan, key)) getOrElse pullUnlimited(chan, key))
+    }), M.point(()))
+  }
 
   def sinkFile[F[_]](f: File)(implicit MR: MonadResource[F]): Sink[ByteString, F, Unit] =
     sinkIOStream(IO(new FileOutputStream(f)))
